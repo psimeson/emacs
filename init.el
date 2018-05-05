ebf41898965d3c65f5784403cbdf88a07e5609a8
Psi Meson <psimeson@gmail.com>
2018-05-05 16:42:47 +1000

    Emacs configuration files

---
+++ init.el
@@ -0,0 +1,16 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+
+(require 'init-ui)
+
+(require 'init-editing)
+
+(require 'init-navigation)
+
+(require 'init-miscellaneous)
+
+(require 'init-company-mode)
+
+(provide 'init)
+
